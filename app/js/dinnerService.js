c09590a6414ddd9dbac05b646287ad8275eac61a
Caroline Arkenson <arkenson@kth.se>
2015-04-21 22:09:39 +0200

    Add dinnerModel to dinnerService

--- app/js/dinnerService.js
+++ app/js/dinnerService.js
@@ -4,24 +4,187 @@
 // service is created first time it is needed and then just reuse it
 // the next time.
 dinnerPlannerApp.factory('Dinner',function ($resource) {
-  
-  var numberOfGuest = 2;
+
+    //Fields
+    var numberOfGuests = 1;
+    var menu = [];
+    var selectedDishID = 1;
+
+    this.getSelectedDishID = function () {
+        return selectedDishID;
+    }
+
+    this.setSelectedDIshID = function (ID) {
+        selectedDishID = ID;
+    }
+
+    //Sets the number of guests
+    this.setNumberOfGuests = function (num) {
+        numberOfGuests = num;
+    }
+
+    //Returns the number of guests
+    this.getNumberOfGuests = function () {
+        return numberOfGuests;
+    }
+
+    //Returns the dish that is on the menu for selected type 
+    this.getSelectedDish = function (category) {
+        for (i in menu) {
+            if (menu[i].Category == category) {
+                return menu[i];     // Only returns a menu item if found, otherwise nothing is returned.
+            }
+        }
+    }
+
+    //Returns all the dishes on the menu (or the selected type, nothing if selected type not found).
+    this.getFullMenu = function () {
+        return menu;
+    }
+
+    //Returns all ingredients for all the dishes on the menu.
+    this.getAllIngredients = function (category) {
+        var ingredients = [];
+        for (i in menu) {
+            if (category == undefined) {
+                for (var j = 0; j < menu[i].Ingredients.length; j++) {
+                    ingredients.push(menu[i].Ingredients[j]);
+                }
+            }
+            if (menu[i].Category == category) {
+                for (var j = 0; j < menu[i].Ingredients.length; j++) {
+                    ingredients.push(menu[i].Ingredients[j]);
+                }
+            }
+        }
+        return ingredients;     // Returns an empty list if no item of that type on menu, or a list with all the ingredients for that item on the menu.
+    }
+
+    //Returns the total price of the menu (all the ingredients multiplied by number of guests).
+    this.getTotalMenuPrice = function (category) {
+        var totalPrice = 0;
+        var ingredients = this.getAllIngredients(category);
+        for (var i = 0; i < ingredients.length; i++) {
+            totalPrice += ingredients[i].Quantity;
+        }
 
 
-  this.setNumberOfGuests = function(num) {
-    numberOfGuest = num;
-  }
+        return (parseFloat(totalPrice).toFixed(2) * numberOfGuests);  // Returns the total cost of all ingredients, is >= 0.
+    }
 
-  this.getNumberOfGuests = function() {
-    return numberOfGuest;
-  }
+    //Adds the passed dish to the menu. If the dish of that type already exists on the menu
+    //it is removed from the menu and the new one added.
+    this.addDishToMenu = function (id) {
+        var apiKey = "dvxkRYZj71vL8irJQo33bFG3o6U34O8K";
+        var url = "http://api.bigoven.com/recipe/"
+                  + id
+                  + "?api_key=" + apiKey;
+        $.ajax({
+            type: "GET",
+            dataType: 'json',
+            cache: false,
+            url: url,
+            success: function (data) {   
 
 
-  // TODO in Lab 5: Add your model code from previous labs
-  // feel free to remove above example code
-  // you will need to modify the model (getDish and getAllDishes) 
-  // a bit to take the advantage of Angular resource service
-  // check lab 5 instructions for details
+        console.log("1. Adding a dish")
+        var found = false;
+        for (i in menu) {
+            if (menu[i].Category == data.Category) {
+                menu.pop(menu[i]);
+                menu.push(data)
+                found = true;
+                console.log("2. Added the dish (already existed) ", data.Title)
+            }
+        }
+        if (!found) {
+            console.log(menu)
+            menu.push(data);
+            console.log("2. Added the dish (newly added)", data.Title)
+            console.log(menu)
+        }
+
+            }
+        });
+
+    }
+
+    //Returns the number of items in menu
+    this.getLengthOfMenu = function () {
+        return menu.length;
+    }
+
+    //Removes dish from menu
+    this.removeDishFromMenu = function (id) {
+        menu.pop(dishes[id]);
+    }
+
+    //function that returns all dishes of specific type (i.e. "starter", "main dish" or "dessert")
+    //you can use the filter argument to filter out the dish by name or ingredient (use for search)
+    //if you don't pass any filter all the dishes will be returned
+    this.getAllDishes = function (type, filter) {
+        return $(dishes).filter(function (index, dish) {
+            var found = true;
+            if (filter) {
+                found = false;
+                $.each(dish.ingredients, function (index, ingredient) {
+                    if (ingredient.name.indexOf(filter) != -1) {
+                        found = true;
+                    }
+                });
+                if (dish.name.indexOf(filter) != -1) {
+                    found = true;
+                }
+            }
+            return dish.type == type && found;
+        });
+    }
+
+    //function that returns a dish of specific ID
+    this.getDish = function (id, callback, view) {
+        var apiKey = "dvxkRYZj71vL8irJQo33bFG3o6U34O8K";
+        var url = "http://api.bigoven.com/recipe/"
+                  + id
+                  + "?api_key=" + apiKey;
+        $.ajax({
+            type: "GET",
+            dataType: 'json',
+            cache: false,
+            url: url,
+            success: function (data) {
+                console.log(data);
+                return callback(data, view);               
+            }
+        });
+
+    }
+
+//title. category är type - main dish, desserts, appetizers. 
+
+//getRecipeJson hämtar nu ut recipe beroende på vad användaren skriver för sökterm och vald food type. 
+
+    this.getRecipeJson = function (term,type,callback,view) {
+               
+        var category = type;
+        var apiKey = "dvxkRYZj71vL8irJQo33bFG3o6U34O8K";
+        var titleKeyword = term;
+        var url = "http://api.bigoven.com/recipes?pg=1&rpp=10&title_kw="
+                  + titleKeyword 
+                  + "&any_kw="
+                  + category
+                  + "&api_key="+apiKey;
+        $.ajax({
+            type: "GET",
+            dataType: 'json',
+            cache: false,
+            url: url,
+            success: function (data) {
+                //run callback
+                return callback(data.Results,view);
+            }
+        });
+    }
+
 
 
 
